de55937b9fbc3bd39732fc5df695a0d3840b83ab
Raj Srivastva <rajsrivastva@Rajs-MacBook-Air-4.local>
2025-04-17 12:41:35 +0530

    updated name

--- frontend/src/components/AppDownload.jsx
+++ frontend/src/components/AppDownload.jsx
@@ -10,7 +10,7 @@ const AppDownload = () => {
 
   return (
     <div className='m-auto mt-24 text-center font-semibold'>
-      <p>For Better Experience Download <br /> Tomato App</p>
+      <p>For Better Experience Download <br />Biterush App</p>
       <div className="flex justify-center items-center gap-2 mt-4">
         <img onClick={msgFunction} className='h-12 cursor-pointer hover:scale-105 transition-all duration-500' src={assets.play_store} alt="" />
         <img onClick={msgFunction} className='h-12 cursor-pointer hover:scale-105 transition-all duration-500' src={assets.app_store} alt="" />
